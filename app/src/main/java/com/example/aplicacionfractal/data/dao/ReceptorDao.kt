e0d0237688aec876c6f1ded0e2869bf917c33a9d
Juan Esteban <jestebanl88m@gmail.com>
2025-02-06 10:43:34 +0100

    Creación de clase y dao para receptor

---
+++ app/src/main/java/com/example/aplicacionfractal/data/dao/ReceptorDao.kt
@@ -0,0 +1,32 @@
+package com.example.aplicacionfractal.data.dao
+
+import com.example.aplicacionfractal.data.models.Receptor
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+class ReceptorDao(private val db: FirebaseFirestore) {
+
+    private val receptoresRef = db.collection("Receptor")
+
+    suspend fun obtenerReceptores(): List<Receptor> {
+        val querySnapshot = receptoresRef.get().await()
+        return querySnapshot.documents.map { it.toObject(Receptor::class.java)!! }
+    }
+
+    suspend fun obtenerReceptor(id: String): Receptor? {
+        val documentSnapshot = receptoresRef.document(id).get().await()
+        return documentSnapshot.toObject(Receptor::class.java)
+    }
+
+    suspend fun agregarReceptor(receptor: Receptor) {
+        receptoresRef.add(receptor).await()
+    }
+
+    suspend fun actualizarReceptor(id: String, receptor: Receptor) {
+        receptoresRef.document(id).set(receptor).await()
+    }
+
+    suspend fun borrarReceptor(id: String) {
+        receptoresRef.document(id).delete().await()
+    }
+}
---
+++ app/src/main/java/com/example/aplicacionfractal/data/models/Receptor.kt
@@ -0,0 +1,7 @@
+package com.example.aplicacionfractal.data.models
+
+data class Receptor(
+    val direccionReceptor: List<String>,
+    val cliente: String,
+    val cif: String
+)

223fd2966c47b6af45671e60976872f09b1764fb
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2025-02-07 18:50:42 +0100

    tabulador facturas emitidas/recibidas

--- app/src/main/java/com/example/aplicacionfractal/screens/pantallaPrincipal.kt
+++ app/src/main/java/com/example/aplicacionfractal/screens/pantallaPrincipal.kt
@@ -15,32 +15,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -54,13 +44,13 @@ import androidx.navigation.NavHostController
 import com.example.aplicacionfractal.data.models.Emisor
 import com.example.aplicacionfractal.data.models.Factura
 import com.example.aplicacionfractal.data.models.Receptor
+import com.example.aplicacionfractal.ui.theme.ColorPrimario
 import com.example.aplicacionfractal.utils.MenuPrincipal
+import com.example.aplicacionfractal.utils.TabMultiple
 import com.example.aplicacionfractal.viewModels.FacturaViewModel
-import kotlinx.coroutines.launch
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.text.SimpleDateFormat
-import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 
@@ -70,10 +60,7 @@ fun PantallaPrincipal(
     navController: NavHostController,
     facturaViewModel: FacturaViewModel = viewModel()
 ) {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-
-    MenuPrincipal(navController = navController) { paddingValues ->
+    MenuPrincipal(navController = navController) {
         Scaffold(
             topBar = {
                 CenterAlignedTopAppBar(
@@ -89,7 +76,56 @@ fun PantallaPrincipal(
                 verticalArrangement = Arrangement.Top
             ) {
                 Spacer(modifier = Modifier.size(5.dp))
-                ListadoFacturas(facturaViewModel)
+                TabMultiple(facturaViewModel)
+            }
+        }
+    }
+}
+
+@Composable
+fun ListadoFacturasRecibidas(facturaViewModel: FacturaViewModel) {
+    val facturas by facturaViewModel.facturas.collectAsState()
+    val facturasRecibidas = facturas.filter { !it.emitida }
+
+    ListadoFacturasGenerico(
+        facturas = facturasRecibidas,
+        facturaViewModel = facturaViewModel,
+        emptyMessage = "No hay facturas recibidas disponibles"
+    )
+}
+
+@Composable
+fun ListadoFacturasEmitidas(facturaViewModel: FacturaViewModel) {
+    val facturas by facturaViewModel.facturas.collectAsState()
+    val facturasEmitidas = facturas.filter { it.emitida }
+
+    ListadoFacturasGenerico(
+        facturas = facturasEmitidas,
+        facturaViewModel = facturaViewModel,
+        emptyMessage = "No hay facturas emitidas disponibles"
+    )
+}
+@Composable
+fun ListadoFacturasGenerico(
+    facturas: List<Factura>,
+    facturaViewModel: FacturaViewModel,
+    emptyMessage: String
+) {
+    if (facturas.isEmpty()) {
+        Text(emptyMessage, Modifier.padding(16.dp))
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(facturas) { factura ->
+                FacturaItem(
+                    factura,
+                    facturaViewModel = facturaViewModel,
+                    onEdit = { /* Lógica para editar */ },
+                    onDelete = { facturaViewModel.eliminarFactura(factura) }
+                )
             }
         }
     }
@@ -235,4 +271,3 @@ fun FacturaItem(
         )
     }
 }
-

dc4ace3caabe63b48acdb198e3ffc23767334721
Paco Solana <fsolanaaranega@gmail.com>
2025-02-19 12:01:10 +0100

    Mejora de función de factura
    He añadido los datos del emisor y receptor, también he estilizado la factura estructurándola con tablas .

--- app/src/main/java/com/example/aplicacionfractal/data/models/Factura.kt
+++ app/src/main/java/com/example/aplicacionfractal/data/models/Factura.kt
@@ -1,34 +1,34 @@
-package com.example.aplicacionfractal.data.models
-import com.google.firebase.Timestamp
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.IgnoreExtraProperties
-import com.google.firebase.firestore.PropertyName
-import java.time.LocalDateTime
+    package com.example.aplicacionfractal.data.models
+    import com.google.firebase.Timestamp
+    import com.google.firebase.firestore.DocumentReference
+    import com.google.firebase.firestore.IgnoreExtraProperties
+    import com.google.firebase.firestore.PropertyName
+    import java.time.LocalDateTime
 
-@IgnoreExtraProperties
-data class Factura(
-    @get:PropertyName("IVA") @set:PropertyName("IVA")
-    var IVA: Double = 0.0,
+    @IgnoreExtraProperties
+    data class Factura(
+        @get:PropertyName("IVA") @set:PropertyName("IVA")
+        var IVA: Double = 0.0,
 
-    @get:PropertyName("baseImponible") @set:PropertyName("baseImponible")
-    var baseImponible: Double = 0.0,
+        @get:PropertyName("baseImponible") @set:PropertyName("baseImponible")
+        var baseImponible: Double = 0.0,
 
-    @get:PropertyName("emisorId") @set:PropertyName("emisorId")
-    var emisorId: DocumentReference? = null,
+        @get:PropertyName("emisorId") @set:PropertyName("emisorId")
+        var emisorId: DocumentReference? = null,
 
-    @get:PropertyName("receptorId") @set:PropertyName("receptorId")
-    var receptorId: DocumentReference? = null,
+        @get:PropertyName("receptorId") @set:PropertyName("receptorId")
+        var receptorId: DocumentReference? = null,
 
-    @get:PropertyName("fechaEmision") @set:PropertyName("fechaEmision")
-    var fechaEmision: Timestamp? = null,
+        @get:PropertyName("fechaEmision") @set:PropertyName("fechaEmision")
+        var fechaEmision: Timestamp? = null,
 
-    @get:PropertyName("nFactura") @set:PropertyName("nFactura")
-    var nFactura: String = "",
+        @get:PropertyName("nFactura") @set:PropertyName("nFactura")
+        var nFactura: String = "",
 
-    @get:PropertyName("total") @set:PropertyName("total")
-    var total: Double = 0.0,
+        @get:PropertyName("total") @set:PropertyName("total")
+        var total: Double = 0.0,
 
-    @get:PropertyName("emitido") @set:PropertyName("emitido")
-    var emitida: Boolean=false
-)
+        @get:PropertyName("emitido") @set:PropertyName("emitido")
+        var emitida: Boolean=false
+    )
 
